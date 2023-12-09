7b0c6af2c60ca081df7b9e1c20dccc9ae3f3096a
adriaan1313 <adriaan1313@outlook.com>
2023-12-09 16:16:33 +0100

    ETISCH TUSSEN 1870 en 1920
    veel dingen van de laatste paar dagen

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
 const {NNet, Laag} = require("./classjes");
+
 // Okeeee, nou wat? Ik kan modellen maken enz, maar dat helpt niet, ik moet ze kunnen trainen.
 const pngParser = require("pngjs").PNG.sync; // ik ga niet helemaal zelf een png-parser schrijven, dat is nu niet is scope voor dit pws
 const fs = require("node:fs");
 
+const aantalRondes = 30000;
+const aantalModellen = 32;
+const aantalKeerUitvoerenPerRonde = 600;
+const mutatieSchaal = 40;
+const dataset = "testData3.png";
+const initGroottes = [3,13,16,16,16,20,16,16,1];
 
-const aantalRondes = 10000;
-const aantalModellen = 16;
-const aantalKeerUitvoerenPerRonde = 10;
 
-
-const testAfbeelding = pngParser.read(fs.readFileSync("testData.png"))
+let bestandsnaam;
+const testAfbeelding = pngParser.read(fs.readFileSync(dataset))
 //onsole.log(testAfbeelding.data)
 const testData = new Array(testAfbeelding.data.length/4);
 for (let i= 0; i < testData.length; i++){
 	
-	testData[i]=testAfbeelding.data[2+i*4]/255;
+	testData[i]=2*testAfbeelding.data[1+i*4]/255;
 }
 // maak een hele boel modellen met random waarden, een hele boel is blijkbaar 6.
-
-const modellen = new Array(aantalModellen)
-for (let i= 0; i < aantalModellen; i++){
-	modellen[i] = {net: new NNet([2,13,16,16,16,1]), score: 0}//voor de testdata: 2 invoerwaarden: x en y, en één uitvoerwaarde, hopelijk de waarde van het blauwe kanaal van de afbeelding die de testdata vormt. Andere waarden willekeurig gekozen.
+const modellen = new Array();
+if(process.argv.length <= 2){
+	for (let i= 0; i < aantalModellen; i++){
+		modellen[i] = {net: new NNet(initGroottes), score: 0}//voor de testdata: 2 invoerwaarden: x en y, en één uitvoerwaarde, hopelijk de waarde van het groene kanaal van de afbeelding die de testdata vormt. Andere waarden willekeurig gekozen.
+	}
+	bestandsnaam = undefined;
+}else {
+	bestand = fs.readFileSync(process.argv[2],  {encoding: "latin1"});
+	bestandsnaam = process.argv[2];
+	const modelTeksten = bestand.split("Een model met ");
+	modelTeksten.shift();
+	for (let i= 0; i < modelTeksten.length; i++){
+		modellen[i] = {net: new NNet(null, modelTeksten[i]), score: 0}//voor de testdata: 2 invoerwaarden: x en y, en één uitvoerwaarde, hopelijk de waarde van het groene kanaal van de afbeelding die de testdata vormt. Andere waarden willekeurig gekozen.
+	}
 }
-let scores = "";
+let spec="";
+if(bestandsnaam)spec=`, vorig_bestand=${bestandsnaam}`;
+else spec=`, init_groottes=${initGroottes}`;
+
+let scores = `rondes=${aantalRondes}, modellen=${modellen.length}, score_gebaseerd_op=${aantalKeerUitvoerenPerRonde}, mutatieschaal=${mutatieSchaal}, dataset=${dataset}${spec}\n`;
 for (let i= 1; i <= aantalRondes; i++){
 	//voer uit en evalueer resultaten
 	let scoreGemiddelde =0; 
 	//let scoreMax = -Infinity;
 	//let scoreMin = Infinity;
 	scores += '\n'+i;
-	for (let j= 0; j < aantalModellen; j++){
-		modellen[j].score=0;
-		
-		for (let k = 0; k<aantalKeerUitvoerenPerRonde; k++){
-			const invoerWaarden = [Math.floor(Math.random()*testAfbeelding.width),Math.floor(Math.random()*testAfbeelding.height)];
-			const verwachteUitvoer = testData[invoerWaarden[0]+testAfbeelding.width*invoerWaarden[1]];
-			const uitvoerModel = clip(modellen[j].net.berekenWaardenVoorInvoer(invoerWaarden));
-			const error=Math.abs(verwachteUitvoer - uitvoerModel)
-			modellen[j].score+= error/*+(Math.random()*.2-.1)*/;
-			scoreGemiddelde+=error;
-		}
-		//scoreMax = Math.max(scoreMax, modellen[j].score);
-		//scoreMin = Math.min(scoreMin, modellen[j].score);
-		scores += ", " + modellen[j].score;
-	}
-	modellen
-	modellen.sort(sorteerFunctie);
+	
+	
 	for (let j = 0; j < (aantalModellen-1)/2; j++){ //laagste helft vermoorden en vervangen met bovenste helft
 		delete modellen[j].net;
 		//console.log(j, modellen[j+Math.round((aantalModellen-1)/2)].net)
 		modellen[j].net = new NNet(null, modellen[j+Math.round((aantalModellen-1)/2)].net.tekst); //veranderen in een string en dan weer in een netwerk, anders wordt het een pointer, en dat willen we niet
 	}
 	//nu moeten we ze kruisen, dit is wat moeilijker, want ik weet niet hoe ik dit het beste kan doen
-	modellen.sort(sorteerShuffle);
+	//modellen.sort(sorteerShuffle);
+	shuffle(modellen);
 	for (let j = 0; j < aantalModellen; j+=2){ 
 		if(modellen[j+1]){ //voorkomen dat we iets doms doen
 			kruis(modellen[j].net, modellen[j+1].net);
@@ -64,24 +68,46 @@ for (let i= 1; i <= aantalRondes; i++){
 		muteer(modellen[j].net);
 		
 	}
+	
+	for (let j= 0; j < aantalModellen; j++){
+		modellen[j].score=0;
+		let bscore = 0;
+		for (let k = 0; k<aantalKeerUitvoerenPerRonde; k++){
+			const x =Math.floor(Math.random()*testAfbeelding.width);
+			const y =Math.floor(Math.random()*testAfbeelding.height);
+			const invoerWaarden = [x,y,1];// extra 1 om gewoon een constante te geven want dat maakt dingen mogelijk beter, ws niet
+			const verwachteUitvoer = testData[x+testAfbeelding.width*y];
+			const uitvoerModel = clip(modellen[j].net.berekenWaardenVoorInvoer(invoerWaarden));
+			const error=Math.abs(verwachteUitvoer - uitvoerModel)
+			modellen[j].score+= error**2/*+(Math.random()*.2-.1)*/;
+			bscore+=error
+			scoreGemiddelde+=error;
+		}
+		//scoreMax = Math.max(scoreMax, modellen[j].score);
+		//scoreMin = Math.min(scoreMin, modellen[j].score);
+		modellen[j].score=(modellen[j].score/aantalKeerUitvoerenPerRonde)**.5;
+		scores += ", " + bscore;
+	}
+	modellen.sort(sorteerFunctie);
 	//const i=0;
 	//if(i%50 == 0) console.log(`${i}e ronde, gem: ${scoreGemiddelde/aantalModellen/aantalKeerUitvoerenPerRonde}, min: ${scoreMin/aantalKeerUitvoerenPerRonde}, max: ${scoreMax/aantalKeerUitvoerenPerRonde}`);
 	if(i%50 == 0) console.log(`${i}e ronde, gem: ${scoreGemiddelde/aantalModellen/aantalKeerUitvoerenPerRonde}`);
 	//console.log(modellen[0].net.lagen[0]);
 }
-
-resultaatTekst = "";
+const dn = Date.now();
+let resultaatTekst = "";
 modellen.forEach((model)=>{resultaatTekst+=model.net.tekst}); //we hoeven nu niet heel efficiënt te zijn, we kunnen dus forEach gebruiken.
-fs.writeFileSync(`modellen-${Date.now()}.txt`, resultaatTekst);
+fs.writeFileSync(`${bestandsnaam||"modellen"}-${dn}.txt`, resultaatTekst, {encoding: "latin1"});
 
 
-fs.writeFileSync(`statistiek-${Date.now()}.csv`, scores, {encoding: "latin1"});
+fs.writeFileSync(`statistiek-${bestandsnaam||""}${dn}.csv`, scores, {encoding: "latin1"});
 
 
 /** functiedefinities */ 
 /*  functiedefinities */
 // niet dingen hierna gaan zetten!!!!
 function kruis(model,b){
+	/*
 	for (let i = 0; i<model.lagen.length; i++){
 		for (let j = 0; j<model.lagen[i].biases.length; j++){
 			if(Math.random()>=.5){
@@ -93,22 +119,36 @@ function kruis(model,b){
 				model.lagen[i].wegingen[j] = b.lagen[i].wegingen[j];
 			}
 		}
+	}*/
+	for (let i = 0; i<model.lagen.length; i++){
+		for (let j = 0; j<model.lagen[i].biases.length; j++){
+			model.lagen[i].biases[j] = interpoleer(model.lagen[i].biases[j], b.lagen[i].biases[j], Math.random());
+		}
+		for (let j = 0; j<model.lagen[i].wegingen.length; j++){
+			model.lagen[i].wegingen[j] =interpoleer(model.lagen[i].wegingen[j], b.lagen[i].wegingen[j], Math.random());
+		}
 	}
 	// hoef niet te returnen want het is muteert het object, maakt er geen kopie van.
 	
 }
 
 function muteer(model) {
+	
 	//console.log(model.lagen[0].wegingen)
 	for (let i = 0; i<model.lagen.length; i++){
 		for (let j = 0; j<model.lagen[i].biases.length; j++){
-			model.lagen[i].biases[j]+=(Math.random()-.5)*.04; //weer gewoon willekeurig getal genomen
+			model.lagen[i].biases[j]+=(Math.random()-.5)*mutatieSchaal; //weer gewoon willekeurig getal genomen
 		}
 		for (let j = 0; j<model.lagen[i].wegingen.length; j++){
-			model.lagen[i].wegingen[j]+=(Math.random()-.5)*.04;
+			model.lagen[i].wegingen[j]+=(Math.random()-.5)*mutatieSchaal;
 		}
 	}
 	//console.log(model.lagen[0].wegingen)
+}
+
+
+function interpoleer(a,b,w){
+	return a*w+b*(1-w);
 }
 
 function clip(a){
@@ -121,6 +161,18 @@ function clip(a){
 function sorteerShuffle(a,b){
 	return Math.random()-.5; //niet werkelijk random, maar omdat het er nu maar zes zijn, is dit goed genoeg.
 }
+
+//https://stackoverflow.com/a/2450976
+function shuffle(array) {
+	let currentIndex = array.length,  randomIndex;
+	while (currentIndex > 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+	return array;
+}
+
 
 function sorteerFunctie(a,b){
 	return b.score-a.score;
